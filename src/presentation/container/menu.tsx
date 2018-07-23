bbe893918834c5bacca6e808d1da86b0c3419c96
akiho <akiho@akiho-macbookpro.local>
2018-07-23 22:55:00 +0900

    follow link

--- src/presentation/container/menu.tsx
+++ src/presentation/container/menu.tsx
@@ -9,9 +9,12 @@ import {createMenuActionCreator} from "../action/menu_action";
 import {css} from "glamor";
 import {Address} from "../../domain/model/address";
 import {Currency} from "../../domain/model/currency";
+import {User} from "../../domain/model/user";
+import {Link} from 'react-router';
 
 interface IProps {
   authState: AuthState;
+  user: User | null;
   state: MenuState;
   dispatcher: IMenuDispatcher;
 }
@@ -40,7 +43,7 @@ class Menu extends React.Component<IProps, IState> {
 
   public render(): JSX.Element {
     const authState = this.props.authState;
-    const { asset, addresses } = this.props.state;
+    const {asset, addresses} = this.props.state;
 
     if (authState === AuthState.LOGIN_USER && !asset) {
       this.props.dispatcher.getAsset();
@@ -52,15 +55,46 @@ class Menu extends React.Component<IProps, IState> {
 
     return (
       <div {...menu}>
+        {this.createUserSection()}
         {this.createAssetSection()}
         {this.createAddressSection()}
       </div>
     );
   }
 
+  private createUserSection(): JSX.Element | null {
+    const authState = this.props.authState;
+    const {user} = this.props;
+
+    if (authState === AuthState.LOGIN_USER && user) {
+      return (
+        <div className="row" style={{padding: 10, margin: 0}}>
+          <div className="col s12 m12">
+            <div className="card blue-grey darken-1" style={{padding: 0, margin: 0}}>
+              <div className="card-content row valign-wrapper" style={{marginBottom: 0, padding: 10}}>
+                <div className="col s3">
+                  <img src={user.iconURL} alt="" className="circle responsive-img"/>
+                </div>
+                <div className="col s9">
+                  <span className="white-text">{user.username}</span>
+                </div>
+              </div>
+              <div className="card-action white-text">
+                <Link to='/' {...link}>follows</Link>
+                <Link to='/' {...link}>followers</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+
   private createAssetSection(): JSX.Element | null {
     const authState = this.props.authState;
-    const { asset } = this.props.state;
+    const {asset} = this.props.state;
 
     if (authState === AuthState.LOGIN_USER && asset) {
       return (
@@ -82,10 +116,10 @@ class Menu extends React.Component<IProps, IState> {
       return null;
     }
   }
-  
+
   private createAddressSection(): JSX.Element | null {
     const authState = this.props.authState;
-    const { addresses, currencies } = this.props.state;
+    const {addresses, currencies} = this.props.state;
 
     if (authState === AuthState.LOGIN_USER && addresses) {
       return (
@@ -138,7 +172,7 @@ class Menu extends React.Component<IProps, IState> {
     return items.map((item, i) => {
       return (
         <div key={i} {...addressList}>
-          <p>{item.code}　[<a href="#" onClick={this.deleteAddress.bind(this, item)} {...deleteAddressButton}>削除</a>]</p>
+          <p>{item.code} [<a href="#" onClick={this.deleteAddress.bind(this, item)} {...deleteAddressButton}>削除</a>]</p>
           <p>{item.text}</p>
         </div>
       );
@@ -194,6 +228,7 @@ class Menu extends React.Component<IProps, IState> {
 const mapStateToProps = (state: RootState): Partial<IProps> => {
   return {
     authState: state.appReducer.authState,
+    user: state.appReducer.user,
     state: state.menuReducer,
   } as Partial<IProps>;
 };
@@ -220,4 +255,8 @@ const deleteAddressButton = css({
 
 const form = css({
   height: 200,
+});
+
+const link = css({
+  color: "#ffab40",
 });
